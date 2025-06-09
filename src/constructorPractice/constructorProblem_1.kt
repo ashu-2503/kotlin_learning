9e028ef3c8f9a957a1afd46f2ddef2d8248a9586
Ashutosh Kumar <aashutosh.blaze@gmail.com>
2025-06-09 15:04:14 +0530

    Constructor Practice 1

--- src/constructorPractice/constructorProblem_1.kt
+++ src/constructorPractice/constructorProblem_1.kt
@@ -1,4 +1,6 @@
-/*Challenge: Enhanced Student Class
+/*
+Challenge: Enhanced Student Class
+
 🧾 Requirements:
 You need to create a class Student that supports:
 
@@ -18,29 +20,29 @@ If either email or age is invalid:
 Skip assigning them
 Print a message like Invalid email or Invalid age
 Use null as the default for unassigned fields like email and age.
- */
+*/
 
+package constructorPractice
 
 fun main() {
-    val s1 = Student("Shashi",101)
-    val s2 = Student("Udit",102,"udit@gmail.com",27)
-    val s3 = Student("Amit",103,"amit@gmail.com",3)
-
+    val s1 = Student("Shashi", 101)
+    val s2 = Student("Udit", 102, "udit@gmail.com", 27)
+    val s3 = Student("Amit", 103, "amit@gmail.com", 3)
 }
 
 class Student {
     var name: String
     var rollNumber: Int
-    var email : String? = null
-    var age :Int? = null
+    var email: String? = null
+    var age: Int? = null
 
-    constructor(name:String,rollNumber: Int){
+    constructor(name: String, rollNumber: Int) {
         this.name = name
         this.rollNumber = rollNumber
         println("Student with name : $name and roll: $rollNumber is created")
     }
 
-    constructor(name:String,rollNumber: Int,email: String,age: Int):this(name,rollNumber) {
+    constructor(name: String, rollNumber: Int, email: String, age: Int) : this(name, rollNumber) {
         if (email.contains("@") && age >= 5 && age <= 100) {
             this.email = email
             this.age = age
